b00c7d44d0d405d9b8778625ed36149e0679c91f
Eirik Høyheim <eirihoyh@nmbu.no>
2020-12-22 16:07:23 +0100

    made a pre made board and made it possible to input own boards

--- sudoku.py
+++ sudoku.py
@@ -7,5 +7,25 @@ class Solve_sudoku:
              [8, 9, 0, 0, 0, 5, 1, 0, 4],
              [6, 3, 0, 0, 4, 0, 0, 5, 7],
              [0, 0, 0, 0, 9, 0, 0, 0, 0],
-             [0, 0, 0, 1, 0, 0, 3, 0, 0],
-             ]
+             [0, 0, 0, 1, 0, 0, 3, 0, 0]]
+
+    def __init__(self, board=None):
+        if board == None:
+            self.board = board
+        else:
+            row_length = len(board[0])
+            i = 0
+            for row in board:
+                if len(row) != row_length:
+                    raise RuntimeError("Board must have equal row length")
+                i += 1
+
+            if i == row_length:
+                self.board = board
+                print(board)
+            else:
+                raise RuntimeError("Board must be quadratic")
+
+
+if __name__ == '__main__':
+    solver = Solve_sudoku()
