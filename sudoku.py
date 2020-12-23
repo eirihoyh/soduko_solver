2367b26f57722949c9aab6ebd09cd19408844ff2
Eirik Høyheim <eirihoyh@nmbu.no>
2020-12-23 15:53:28 +0100

    made it possible to solve sudoku
    solves soduko by taking all empty places and trying to replace the empty spaces with numbers between 1 and 9. This will happen to all places until there is no empty spaces left.  Also changed how we found empty spaces and how we checked what numbers is allready used

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Solve_sudoku:
     board = [[3, 5, 0, 9, 0, 6, 0, 1, 0],
              [0, 0, 0, 0, 0, 0, 0, 9, 0],
@@ -12,8 +13,7 @@ class Solve_sudoku:
              [0, 0, 0, 1, 0, 0, 3, 0, 0]]
 
     def __init__(self, board=None):
-
-        if board != None:
+        if board is not None:
             row_length = len(board[0])
             i = 0
             for row in board:
@@ -27,31 +27,46 @@ class Solve_sudoku:
             else:
                 raise RuntimeError("Board must be quadratic")
 
-        self.numbers = np.arange(1,10)
-        self.positions = []
+        self.N = len(self.board[0])
+        self.numbers = np.arange(1, self.N + 1)
 
-    def solve(self, pos, used_numbers=0):
-        if used_numbers == 0:
-            used_numbers = [0]
+    def solve(self):
+        empty_places = self.find_empty()
+        if empty_places == []:
+            return True
 
-        usable_numbers = [x for x in self.numbers if x not in used_numbers]
-        for number in usable_numbers:
-            if number != 0:
-                self.board[pos[0]][pos[1]] = number
-            for i, row in enumerate(
-                    self.board):  # This mus be something else than self.board or something
-                for j, col in enumerate(row):
-                    if number == 0:
-                        used_numbers = self.check_row_and_cols([i, j])
-                        self.solve([i, j], used_numbers)
+        for i in self.numbers:
+            taken = self.check_row_and_cols(empty_places[0])
+            usable_numbers = [x for x in self.numbers if x not in taken]
+            if i in usable_numbers:
+                self.board[empty_places[0][0]][empty_places[0][1]] = i
+
+                if self.solve() is True:
+                    return True
+
+                self.board[empty_places[0][0]][empty_places[0][1]] = 0
+
+        return False
+
+    def find_empty(self):
+        empty = []
+        for i, row in enumerate(self.board):
+            for j, col in enumerate(row):
+                if col == 0:
+                    empty.append([i, j])
+
+        return empty
 
     def check_row_and_cols(self, pos):
-        row = [row[pos[0]] for row in self.board]
-        col = self.board[pos[1]]
-        return list(set(col+row))
-
+        col = [col[pos[1]] for col in self.board]
+        row = self.board[pos[0]]
+        no_go = list(set(col + row))
+        if 0 in no_go:
+            no_go.remove(0)
+        return no_go
 
 
 if __name__ == '__main__':
     solver = Solve_sudoku()
-    print(solver.numbers)
+    solver.solve()
+    print(solver.board)
