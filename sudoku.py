1c49abac58d37d20db6438a9cf1dba0057a72c92
Eirik Høyheim <eirihoyh@nmbu.no>
2020-12-23 16:04:36 +0100

    Added comments on functions and made it print our solved board

--- sudoku.py
+++ sudoku.py
@@ -13,6 +13,10 @@ class Solve_sudoku:
              [0, 0, 0, 1, 0, 0, 3, 0, 0]]
 
     def __init__(self, board=None):
+        """
+        :param board:
+            Sudoku board that contains zero
+        """
         if board is not None:
             row_length = len(board[0])
             i = 0
@@ -30,18 +34,23 @@ class Solve_sudoku:
         self.N = len(self.board[0])
         self.numbers = np.arange(1, self.N + 1)
 
-    def solve(self):
+    def try_to_solve(self):
+        """
+        Solves sudoku by using backtracking.
+        :return:
+            Bool, True when done solving and False if not solvable.
+        """
         empty_places = self.find_empty()
         if empty_places == []:
             return True
 
         for i in self.numbers:
-            taken = self.check_row_and_cols(empty_places[0])
+            taken = self.taken_numbers(empty_places[0])
             usable_numbers = [x for x in self.numbers if x not in taken]
             if i in usable_numbers:
                 self.board[empty_places[0][0]][empty_places[0][1]] = i
 
-                if self.solve() is True:
+                if self.try_to_solve() is True:
                     return True
 
                 self.board[empty_places[0][0]][empty_places[0][1]] = 0
@@ -49,6 +58,11 @@ class Solve_sudoku:
         return False
 
     def find_empty(self):
+        """
+        find_empty finds the places that contains zero
+        :return:
+        a list of positions in the board that contains zero
+        """
         empty = []
         for i, row in enumerate(self.board):
             for j, col in enumerate(row):
@@ -57,7 +71,14 @@ class Solve_sudoku:
 
         return empty
 
-    def check_row_and_cols(self, pos):
+    def taken_numbers(self, pos):
+        """
+        Find all number in the same row and same column that is already taken
+        :param pos:
+            Position on the board
+        :return:
+            A list with all numbers that is not usable for the position.
+        """
         col = [col[pos[1]] for col in self.board]
         row = self.board[pos[0]]
         no_go = list(set(col + row))
@@ -65,8 +86,13 @@ class Solve_sudoku:
             no_go.remove(0)
         return no_go
 
+    def solve(self):
+        if not self.try_to_solve():
+            print("This board is not solvable")
+        for row in self.board:
+            print(f"{row}")
+
 
 if __name__ == '__main__':
     solver = Solve_sudoku()
     solver.solve()
-    print(solver.board)
